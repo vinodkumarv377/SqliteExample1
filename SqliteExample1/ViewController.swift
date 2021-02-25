c74d8191886e2e34d478a9b3f7c766e04963032e
Guvvala Vinod Kumar <vinodkumarv377@Achiever-59.local>
2021-02-25 16:57:07 +0530

    Initial project setup

---
+++ SqliteExample1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SqliteExample1
+//
+//  Created by Guvvala Vinod Kumar on 25/02/21.
+//  Copyright © 2021 Guvvala Vinod Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
